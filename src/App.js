d7273d0e55dacee2d377fe36443c42c36f6e4469
Denis Sanchez <deslvvpr@gmail.com>
2025-05-21 00:25:42 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import background from './background_fire.mp4';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <video src={background} autoPlay loop />
+        <div className='App_text-container'>
+          <div className='moon'>🌕</div>
+          <h1>
+          Good Night World...
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           This App is in Process.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
     </div>
   );
 }
